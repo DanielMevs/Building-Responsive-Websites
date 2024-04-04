5c4e1a2a7dac0d16ae8b17d78e0beb8b982e3bfd
DanielMevs <dannymevs@hotmail.com>
2024-04-04 13:55:52 -0400

    implemented sticky navigation

--- omnifood_project_finishing_touches/js/script.js
+++ omnifood_project_finishing_touches/js/script.js
@@ -56,6 +56,29 @@ allLinks.forEach(function (link) {
 console.log(allLinks);
 
 ///////////////////////////////////////////////////////////
+// Stick navigation
+const sectionHeroEle = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEle);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
